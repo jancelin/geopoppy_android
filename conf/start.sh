7c775ff8eb568b54f31e34c19f911edb6202ae82
ANCELIN Julien <julien.ancelin@inra.fr>
2019-09-19 09:16:14 +0200

    Update start.sh

--- conf/start.sh
+++ conf/start.sh
@@ -4,6 +4,6 @@ service postgresql start &&
 service nginx restart &&
 sudo service php7.3-fpm restart &&
 
-qgisserver -c /sdcard/geopoppy/conf/qgisserver.conf --rootdir /
+qgisserver -c /storage/internal/geopoppy/conf/qgisserver.conf --rootdir /
 #wpsserver --logging debug -b 127.0.0.1 -p 8081 -w 1 --chdir /sdcard/geopoppy/processing/
 #sudo redis-server /etc/redis/redis.conf
